1b090503fff7af3872c8d49421283f301f14ced8
WirlyWirly <38897939+WirlyWirly@users.noreply.github.com>
2026-05-14 18:11:42 -0700

    dry run

--- ABSidekickRenamer.py
+++ ABSidekickRenamer.py
@@ -16,10 +16,11 @@ from pathlib import Path
 
 # The arguments parser
 parser = argparse.ArgumentParser(prog='python ABSidekick.py', formatter_class=argparse.RawTextHelpFormatter, description="Organize Audiobookshelf matches using the 'metadata.json' of each item")
+parser.add_argument('-c', '--copy', action="store_true", help='Copy the item folder instead of renaming (moving) it')
+parser.add_argument('-d', '--dry', action="store_true", help='Perform a dry-run, not making any actual changes')
+parser.add_argument('-f', '--format', metavar="'String Format'", help='Specify a format for the output folders\n\n%%asin%% %%author%% %%isbn%% %%language%% %%narrator%% %%publisher%% %%title%% %%year%%\n\n')
 parser.add_argument('-o', '--output', dest='output', metavar='PATH', required=True, help='The output path of the process')
 parser.add_argument('-t', '--tag', metavar='TagName', help='Process only the items that include this tag')
-parser.add_argument('-f', '--format', metavar="'String Format'", help='Specify a format for the output folders\n\n%%asin%% %%author%% %%isbn%% %%language%% %%narrator%% %%publisher%% %%title%% %%year%%\n\n')
-parser.add_argument('-c', '--copy', action="store_true", help='Copy the item folder instead of renaming (moving) it')
 parser.add_argument('-v', '--version', action='version', version=f"Version {__version__}")
 parser.add_argument('input_folders', metavar='Input Folder(s)', nargs='+', help="Folder(s) from where a recursive search for 'metadata.json' will be performed")
 
@@ -32,7 +33,7 @@ if args.output:
     # If the output does not exist or is not valid, offer to create it
     if not test_output.is_dir():
         print(f"\nThe output path does not exist\n\n{test_output}")
-        response = input("\nWould you like to create it? [y\\n]: ")
+        response = input("\nWould you like to create it and continue? [y\\n]: ")
         if response.lower() in ['y', 'yes']:
             test_output.mkdir(parents=True)
             output_folder = test_output
@@ -132,28 +133,33 @@ for metadata_file in all_metadata_files:
     print(f"Old: {current_folder}")
     print(f"New: {new_foldername}")
 
-    try:
+    if args.dry != True:
+        # This is NOT a dry run
 
-        new_foldername.parent.mkdir(parents=True, exist_ok=True)
+        try:
 
-        # Copy the files
-        if args.copy:
-            shutil.copytree(current_folder, new_foldername, dirs_exist_ok=True, copy_function=shutil.copy2)
+            new_foldername.parent.mkdir(parents=True, exist_ok=True)
 
-        # Rename (Move) the files
-        else:
-            current_folder.rename(new_foldername)
+            # Copy the files
+            if args.copy == True:
+                shutil.copytree(current_folder, new_foldername, dirs_exist_ok=True, copy_function=shutil.copy2)
+                print('Success! [Copied]')
 
-            # Check for and delete a empty parent directory
-            # if any(current_folder.parent.iterdir()) == False:
-            #     shutil.rmtree(current_folder.parent)
+            # Rename (Move) the files
+            else:
+                current_folder.rename(new_foldername)
 
+                # Check for and delete a empty parent directory
+                # if any(current_folder.parent.iterdir()) == False:
+                #     shutil.rmtree(current_folder.parent)
+                print('Success! [Moved]')
 
-        print('Success!')
-
-    except Exception as error:
-        print(f"Error: {error}")
-        summary = f"{summary}\nError | {metadata_file}"
+        except Exception as error:
+            print(f"Error: {error}")
+            summary = f"{summary}\nError | {metadata_file}"
+    else:
+        # This IS a dry run
+        print('--- Dry ---')
 
     print('\n---------------------------\n')
 
